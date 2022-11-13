87b87ba0fb3156d15587678c0a6b15dfd17c8818
mishazawa <macintoshkrug@gmail.com>
2022-11-13 18:30:06 +0200

    + added realtime mesh generation
    TODO: get rid last triangle

--- src/app/engine/painter/painter.service.ts
+++ src/app/engine/painter/painter.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@angular/core";
 import {
-  Mesh,
-  MeshBuilder,
-  Scene,
-  StandardMaterial,
-  Vector3,
-  VertexBuffer,
-  VertexData,
-  WebXRInputSource
+    Material,
+    Mesh,
+    Scene,
+    StandardMaterial,
+    Vector3,
+    VertexBuffer,
+    WebXRInputSource
 } from "@babylonjs/core";
 import { ControllerHandler } from "./ControllerHandler";
+
+const SIMULTANEOUS_ADDED_VERTICES = 2;
+const VERTEX_BUFFER_STRIDE = 3;
 
 
 @Injectable({ providedIn: 'root' })
@@ -18,13 +20,15 @@ export class PainterService {
   private scene: Scene;
   private canPaint: boolean;
 
-  public lines: Vector3[][] = [];
-  public currentPoints: Vector3[] = [];
+  public scale = .025;
 
-  public scale = .05;
+  private mat: Material;
 
-  public currentMesh;
-  public mat;
+  private mesh: Mesh;
+  private indices: Uint32Array;
+  private positions: Float32Array;
+  private currentIndex: number = 0;
+  private currentIndexBufferSize: number = 2;
 
   public addScene (scene: Scene) {
     this.scene = scene;
@@ -45,62 +49,92 @@ export class PainterService {
 
   private triggerPaint ({pressed}) {
     this.canPaint = pressed;
+
     if (pressed) {
-      this.currentPoints = [];
-
-    } else {
-      this.lines.unshift(this.currentPoints);
-      const points = this.lines[0];
-
-      const mesh = new Mesh(null, this.scene);
-      const vd   = new VertexData();
-
-      mesh.material = this.mat;
-
-      const vertices = [];
-      const indices  = [];
-
-      let currentIndex = 0;
-      for (let i = 0; i < points.length; i++) {
-          const pos   = points[i];
-          const pnext = points[i+1];
-
-          // TODO: stores only last rotation of controller
-          // not correct
-          const tan = this.rightHand.up.scale(this.scale);
-          const pos_ = pos.add(tan);
-
-          // add current and topmost vertices
-          vertices.push(pos.x, pos.y, pos.z);
-          vertices.push(pos_.x, pos_.y, pos_.z);
-
-          // indices
-          var icurrent = currentIndex;
-          var itop     = currentIndex + 1;
-          var inext    = currentIndex + 2;
-
-          // if not last point
-          if (pnext) {
-              indices.push(icurrent, inext,    itop); // 0, 2, 1
-              indices.push(inext,    inext+1,  itop); // 2, 3, 1
-          }
-
-          // increment index (because added two vertices)
-          currentIndex += 2;
-      }
-
-        // console.log(vertices, indices)
-
-        vd.positions = vertices;
-        vd.indices   = indices;
-
-        vd.applyToMesh(mesh);
+      this.initNewLine()
     }
+  }
+
+  private initNewLine() {
+    this.updateBuffers()
+
+    this.mesh = new Mesh(null);
+    this.mesh.material = this.mat;
+
+    this.mesh.setIndices(this.indices, null, true);
+    this.mesh.setVerticesData(VertexBuffer.PositionKind, this.positions, true, VERTEX_BUFFER_STRIDE);
+
+    this.currentIndex = 0;
   }
 
   private paint(controller: WebXRInputSource) {
     if (!this.canPaint) return;
+
     const pos = controller.pointer.position.clone();
-    this.currentPoints.push(pos);
+    this.addPoint(pos);
+  }
+
+  private addPoint(pos: Vector3) {
+    if (this.isBufferFilled()) {
+      this.resize();
+      this.updateBuffers();
+    }
+
+    const tan = this.rightHand.up.normalize().scale(this.scale);
+    const pos_ = pos.add(tan);
+
+    const lastIndex = this.currentIndex;
+
+    this.pushVertex(pos);
+    this.pushVertex(pos_);
+
+    this.pushTriangle(lastIndex,   lastIndex,   lastIndex+2, lastIndex+1); // ∆ 0 2 1
+    this.pushTriangle(lastIndex+1, lastIndex+2, lastIndex+3, lastIndex+1); // ∆ 2 3 1
+
+    this.mesh.updateIndices(this.indices, 0);
+    this.mesh.setVerticesData(VertexBuffer.PositionKind, this.positions, true, VERTEX_BUFFER_STRIDE);
+
+  }
+
+  private initBuffers () {
+    return [
+      new Float32Array(this.currentIndexBufferSize * VERTEX_BUFFER_STRIDE),
+      new Uint32Array(this.currentIndexBufferSize * VERTEX_BUFFER_STRIDE * SIMULTANEOUS_ADDED_VERTICES)
+    ]
+  }
+
+  private updateBuffers () {
+    const [posBuff, idxBuff] = this.initBuffers();
+
+    posBuff.set(this.positions || []);
+    idxBuff.set(this.indices || []);
+
+    this.indices   = idxBuff as Uint32Array;
+    this.positions = posBuff as Float32Array;
+  }
+
+  private pushVertex (pos: Vector3) {
+    const i = this.currentIndex * VERTEX_BUFFER_STRIDE;
+
+    this.positions[i]   = pos.x;
+    this.positions[i+1] = pos.y;
+    this.positions[i+2] = pos.z;
+
+    this.currentIndex++;
+  }
+
+  private pushTriangle(index, v1, v2, v3) {
+    const i = index * VERTEX_BUFFER_STRIDE;
+    this.indices[i]   = v1;
+    this.indices[i+1] = v2;
+    this.indices[i+2] = v3;
+  }
+
+  public isBufferFilled(): boolean {
+    return this.currentIndexBufferSize <= this.currentIndex + SIMULTANEOUS_ADDED_VERTICES;
+  }
+
+  public resize (mult = 2) {
+    this.currentIndexBufferSize *= mult;
   }
 }
